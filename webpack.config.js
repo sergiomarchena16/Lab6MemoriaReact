376fd39d991158f02854525d00df5d6dc645bd6e
Sergio A. Marchena G <mar16387@uvg.edu.gt>
2019-11-13 16:55:32 -0600

    memoria React final
    final

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "src/index.jsx"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+
+      {
+        test:/\.css$/,
+        use:['style-loader','css-loader']
+      }
+
+    ]
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist"),
+    port: 9000
+  }
+};
